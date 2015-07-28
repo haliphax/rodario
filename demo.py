219deddfa79d7d83aa4e37f8a599f023452b255b
haliphax <haliphax@gmail.com>
2015-07-27 21:39:37 -0500

    more robust demo

--- demo.py
+++ demo.py
@@ -15,12 +15,14 @@ class MyActor(Actor):
     def greeting(self, prefix='The Great'):
         print 'Hello! My name is {prefix} {name}'.format(prefix=prefix,
                                                          name=self.name)
-        return 'return value'
+        return 'return value for %s' % prefix
 
 if __name__ == '__main__':
     actor = MyActor('Rodario', 'rodario')
     actor.start()
     actor.greeting('The Intelligent')
     proxy = ActorProxy(uuid=actor.uuid)
-    return_value = proxy.greeting('The Elegant')
-    print return_value.get()
+    return_value1 = proxy.greeting('The Elegant')
+    return_value2 = proxy.greeting()
+    print return_value2.get()
+    print return_value1.get()
