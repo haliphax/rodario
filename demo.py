40ec2d48af7d32d3275a0e44e865c2d62951e290
haliphax <haliphax@gmail.com>
2015-12-21 11:57:07 -0600

    add cluster channel to demo

--- demo.py
+++ demo.py
@@ -12,6 +12,7 @@ class MyActor(Actor):
     def __init__(self, name, uuid=None):
         self.name = name
         super(MyActor, self).__init__(uuid)
+        self.join('test', self.channel_func)
 
     def greeting(self, prefix='The Great'):
         print 'Hello! My name is {prefix} {name}'.format(prefix=prefix,
@@ -26,9 +27,24 @@ class MyActor(Actor):
 
         return 'return value for {prefix} long greet'.format(prefix=prefix)
 
+    def channel_func(self, message):
+        print 'Cluster message received by %s' % self.name
+
+
+class Orchestrator(Actor):
+
+    _count = 0
+
+    def publish(self, message):
+        self._count = self._redis.publish('cluster:test', 1)
+        print 'Cluster channel subscriber count: %d' % self._count
+
+
 if __name__ == '__main__':
     actor = MyActor('Rodario', 'rodario')
+    actor2 = MyActor('Lesser Actor')
     actor.start()
+    actor2.start()
     actor.greeting('The Intelligent')
     proxy = ActorProxy(uuid=actor.uuid)
     return_value1 = proxy.greeting('The Elegant')
@@ -36,3 +52,6 @@ if __name__ == '__main__':
     return_value2 = proxy.greeting()
     print return_value2.get()
     print return_value1.get()
+    orchestrator = Orchestrator()
+    orchestrator.publish('Test')
+    time.sleep(1)
